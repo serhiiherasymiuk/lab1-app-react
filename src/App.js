72879151b58bd860175772fb11da1c15879d52f8
Serhii Herasymiuk <sgerasimuk07@gmail.com>
2024-03-08 15:53:24 +0200

    Add input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedPhotos, setSelectedPhotos] = useState([]);
+
+    const handleFileSelect = (event) => {
+        const files = Array.from(event.target.files);
+        const newPhotos = files.map(file => URL.createObjectURL(file));
+        setSelectedPhotos(prevPhotos => [...prevPhotos, ...newPhotos]);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Add new photos
+                </p>
+                <input type="file" accept="image/*" multiple onChange={handleFileSelect} /> {/* File input field */}
+                <div className="flex-container">
+                    {selectedPhotos.map((photo, index) => (
+                        <img key={index} src={photo} className="Selected-photo" alt={`Selected Photo ${index + 1}`} />
+                    ))}
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
